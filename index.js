1415158911166c7d9ba243e9e5bff1d3a6728749
mhlehri <lehrimirza101@gmail.com>
2023-10-18 21:19:55 +0600

    product insert process added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://mahmudhassanlehri:mhlehri101@cluster0.yynznjj.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const productsCollection = client.db("ProductsDB").collection("products");
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.post("/addProducts", async (req, res) => {
+      const product = req.body;
+      console.log(product);
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+  } finally {
+    console.log("onk");
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("this is server side...");
+});
+app.listen(port);
