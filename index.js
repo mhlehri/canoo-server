50eeff2c3e69a458e5204e7d1c86c9b52f84910c
mhlehri <lehrimirza101@gmail.com>
2023-12-02 23:48:10 +0600

    updated

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const uri =
-  "mongodb+srv://mahmudhassanlehri:mhlehri101@cluster0.yynznjj.mongodb.net/?retryWrites=true&w=majority";
+app.use(cookieParser());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yynznjj.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,17 +23,36 @@ const client = new MongoClient(uri, {
 });
 const productsCollection = client.db("ProductsDB").collection("products");
 const cartCollection = client.db("ProductsDB").collection("carts");
-
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, "secret", (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 async function run() {
   try {
-    await client.connect();
-
-    app.get("/cars/:brand", async (req, res) => {
-      const brand = req.params.brand;
-      const query = { brand: brand };
+    app.get("/cars", async (req, res) => {
+      const brand = req.query.brand;
+      console.log(brand);
+      const query = {
+        $or: [{ brand: brand }, { name: { $regex: brand, $options: "i" } }],
+      };
       const pro = productsCollection.find(query);
       const result = await pro.toArray();
+      console.log(result);
       res.send(result);
+    });
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign({ user: user }, "secret", { expiresIn: "1h" });
     });
 
     app.get("/cars-info/:carName", async (req, res) => {
@@ -42,6 +65,36 @@ async function run() {
     app.get("/cars-info", async (req, res) => {
       const pro = cartCollection.find();
       const result = await pro.toArray();
+      res.send(result);
+    });
+    app.get("/cars-edit/:carName", async (req, res) => {
+      const name = req.params.carName;
+      const query = { name: name };
+      const result = await productsCollection.findOne(query);
+      res.send();
+    });
+
+    app.put("/cars-edit/:carName", async (req, res) => {
+      const newCar = req.body;
+      const car = req.params.carName;
+      const query = { name: car };
+      const options = { upsert: true };
+      const updateCar = {
+        $set: {
+          name: newCar.name,
+          brand: newCar.brand,
+          type: newCar.type,
+          price: newCar.price,
+          photo: newCar.photo,
+          rating: newCar.rating,
+          des: newCar.des,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        query,
+        updateCar,
+        options
+      );
       res.send(result);
     });
 
@@ -63,11 +116,12 @@ async function run() {
       res.send(result);
     });
   } finally {
+    console.log("run");
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("this is server side...");
+  res.send("Changed.");
 });
 app.listen(port);
